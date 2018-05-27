1683e7526d1f128ca11560082416c8dd692240b8
Shiv Pande <shivknight@gmail.com>
2018-05-27 14:30:58 -0700

    Add and use react-virtualized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import { Collection } from 'react-virtualized'
+import 'react-virtualized/styles.css'; // only needs to be imported once
+
+const numberOfItemsPerRow = 3
+const itemWidth = 300
+const itemHeight = 300
+const itemsPadding = 20
+
+// Collection data as an array of objects
+const list = [
+  { name: 'Brian Vaughn 1'},
+  { name: 'Brian Vaughn 2'},
+  { name: 'Brian Vaughn 3'},
+  { name: 'Brian Vaughn 4'},
+  { name: 'Brian Vaughn 5'},
+  { name: 'Brian Vaughn 6'},
+  { name: 'Brian Vaughn 11'},
+  { name: 'Brian Vaughn 22'},
+  { name: 'Brian Vaughn 33'},
+  { name: 'Brian Vaughn 44'},
+  { name: 'Brian Vaughn 55'},
+  { name: 'Brian Vaughn 66'}
+
+  // And so on...
+];
+
+function cellRenderer ({ index, key, style }) {
+  let borderStyle = { border: '5px solid black' }
+
+  return (
+    <div key={key} style={style}>
+      <p style={{ border: '1px solid black' }}>
+        {list[index].name}
+      </p>
+    </div>
+  )
+}
+
+function cellSizeAndPositionGetter ({ index }) {
+  const row = Math.floor(index / numberOfItemsPerRow)
+  const column = index % numberOfItemsPerRow
+
+  return {
+    width: itemWidth,
+    height: itemHeight,
+    x: itemsPadding + (itemsPadding+itemWidth)*column,
+    y: 2*itemsPadding*row,
+  }
+}
 
 class App extends Component {
   render() {
@@ -13,6 +63,17 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div style={{ border: '30px solid green' }}>
+          <Collection
+            style={{ border: '30px solid blue' }}
+            cellCount={list.length}
+            cellRenderer={cellRenderer}
+            cellSizeAndPositionGetter={cellSizeAndPositionGetter}
+            height={2000}
+            width={2000}
+          />
+        </div>
       </div>
     );
   }
