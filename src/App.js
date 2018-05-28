87f6016d86f4336c6b5d6bdec6fb92b42db4035c
Shiv Pande <shivknight@gmail.com>
2018-05-28 00:20:57 -0700

    Remove headers

--- src/App.js
+++ src/App.js
@@ -39,22 +39,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <div style={{ border: '1px solid green'}}>
-          <Gallery
-            photos = {galleryPhotos}
-            columns = '3'
-            onClick = {photoSelected}
-            ImageComponent = {getImageComponent}
-          />
-        </div>
+        <Gallery
+          photos = {galleryPhotos}
+          columns = '3'
+          onClick = {photoSelected}
+          ImageComponent = {getImageComponent}
+        />
       </div>
     );
   }
